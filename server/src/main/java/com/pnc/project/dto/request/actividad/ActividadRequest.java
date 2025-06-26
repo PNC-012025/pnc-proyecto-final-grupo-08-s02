1ba1d291c44e67134b7366004946c05b354dc899
PedroManzanares14 <00230422@uca.edu.sv>
2025-06-26 01:49:53 -0600

    FIX: fix some bugs after changes 2

--- server/src/main/java/com/pnc/project/dto/request/actividad/ActividadRequest.java
+++ server/src/main/java/com/pnc/project/dto/request/actividad/ActividadRequest.java
@@ -2,6 +2,7 @@ package com.pnc.project.dto.request.actividad;
 
 import com.pnc.project.utils.enums.ActividadNombre;
 import com.pnc.project.utils.enums.RolNombre;
+import com.pnc.project.utils.enums.TipoActividad;
 import com.pnc.project.validation.ActividadRolValida;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -20,5 +21,8 @@ public class ActividadRequest {
 
     @NotNull(message = "El rol del instructor es obligatorio")
     private RolNombre rolInstructor;   // llega del front
+
+    @NotNull(message = "El tipo es obligatorio")
+    private TipoActividad tipoActividad;
 
 }
